b18a9192b534697bc2e7a000609e7795cd00aaff
Prikshat2830 <prikshat384@gmail.com>
2026-02-19 09:20:33 +0530

    javascript started

---
+++ Training/script1.js
@@ -0,0 +1,92 @@
+// console.log("Hello World");
+
+// var a
+// a = 10
+
+// console.log(a)
+
+// let b = 20
+
+// console.log(b)
+
+// const c = 30
+
+// console.log(c)
+
+// function add (x,y) {
+//     console.log(x+y)
+// }
+
+// let x = 10
+// let y = 25
+// add(x,y)
+
+// const sub = function (){
+//     console.log("Hello world")
+// }
+
+// sub()
+
+// let but = () => {
+//     console.log("Hello coder roots")
+// }
+
+// but()
+
+
+// 18-02-2026 
+
+// elementby id
+// const head = document.getElementById("heading")
+
+// console.log(head)
+
+// attribute
+
+// function changeImage(){
+//     // const img = document.getElementById("imgg")
+//     // img.setAttribute("src", "https://images.unsplash.com/photo-1771250557679-52dd03d40d92?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8")
+
+// //     const img = document.getElementById("imgg")
+// // const source = img.getAttribute("src")
+
+// // console.log(source)
+
+//     // const img = document.getElementById("imgg")
+//     // img.removeAttribute("src")
+// }
+
+// textcontent
+
+// function changeContent(){
+    // const head = document.getElementById("heading")
+    // head.textContent = "Bye Bye"
+
+    // inner
+
+    // const head = document.getElementById("heading")
+    // head.innerHTML = "<u>Bye Bye</u>"
+
+    // style
+
+    // const head = document.getElementById("heading")
+    // head.style.color = "red"
+
+    // classlist
+
+    // const head = document.getElementById("heading")
+    // head.classList.add("blue")
+
+    // const head = document.getElementById("heading")
+    // head.classList.remove("blue")
+
+    // const head = document.getElementById("heading")
+    // head.classList.toggle("blue")
+
+
+// }
+
+// const para = document.createElement("p")
+// para.textContent = "this is a new para"
+
+// container.appendChild(para)
