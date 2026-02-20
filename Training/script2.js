dd56e6b942acc2325ae4b9f953179ac5219ebb70
Prikshat2830 <prikshat384@gmail.com>
2026-02-20 10:59:49 +0530

    new files

---
+++ Training/script2.js
@@ -0,0 +1,59 @@
+// const head = document.getElementById("heading")
+// const button = document.getElementById("btn")
+
+// function changeContent() {
+//     head.textContent = "Yellow"
+// }
+
+// button.addEventListener("click", function(){
+//     head.textContent = "Namaste"
+// })
+
+// function changeContent() {
+//     head.textContent = "Sat Shri Akal"
+// }
+
+// button.addEventListener("click", changeContent)
+
+// button.addEventListener("mouseover", function(){
+//     head.textContent = "Namaste"
+// })
+
+// const form = document.getElementById("frm")
+
+// form.addEventListener("submit", function(e){
+//     e.preventDefault()
+//     console.log("form submitted")
+// })
+
+
+// const name = document.getElementById("nme")
+
+// name.addEventListener("input", function(e){
+//     // e.target.value = e.target.value.toUpperCase()
+//     console.log(name.value)
+// })
+
+// name.addEventListener("blur", function(){
+//     alert("Input Field Lost")
+// })
+
+// name.addEventListener("focus", function(){
+//     alert("Input Field Focused")
+// })
+
+// name.addEventListener("change", function(){
+//     alert("Values Changed: " + name.value)
+// })
+
+// const parent = document.getElementById("parent")
+// const child = document.getElementById("child")
+
+// parent.addEventListener("click", function(){
+//     alert("Parent Clicked")
+// })
+
+// child.addEventListener("click", function(e){
+//     e.stopPropagation()
+//     alert("Child Clicked")
+// })
